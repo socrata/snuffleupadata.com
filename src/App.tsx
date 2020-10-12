e262e5ab650caa561b40585ac1da17fd87b9b54d
Dominic Cicilio <dominic.cicilio@tylertech.com>
2020-10-12 16:10:58 -0400

    adds bootstrap, fixes styling imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import 'lightbox2/dist/js/lightbox-plus-jquery'
+import './js/snuffleupadata'
+import HomePage from './pages/home'
+import './App.css'
+import './styles/index.scss'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app' className='App container-fluid content'>
+      <header />
+      <HomePage />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
